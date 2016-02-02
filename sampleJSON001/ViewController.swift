d0c55866adeb5a35ee5ffcdd5189c2a7e972e416
ei17ringo <ei17ringo@gmail.com>
2016-02-02 16:42:09 +0800

    commit init

--- sampleJSON001/ViewController.swift
+++ sampleJSON001/ViewController.swift
@@ -15,6 +15,19 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    override func viewWillAppear(animated: Bool) {
+        // json.txtファイルを読み込んで
+        var path = NSBundle.mainBundle().pathForResource("json", ofType: "txt")
+        var jsondata = NSData(contentsOfFile: path!)
+        
+        // 配列データに変換して
+        let jsonArray = (try! NSJSONSerialization.JSONObjectWithData(jsondata!, options: [])) as! NSArray
+        
+        //配列の個数だけ繰り返して表示する
+        for dat in jsonArray{
+            print("値=[\(dat)]")
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
